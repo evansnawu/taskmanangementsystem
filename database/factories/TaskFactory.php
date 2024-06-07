394306e13e9b469f3b9ed37209be4deb3b1f6316
Evans Nawu <evans.nawu@wconsulting.mu>
2024-06-07 23:19:38 +0400

    feat: add tast factor and seeder

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $arrayValues = ['Pending', 'In Progress', 'Completed'];
+
+        return [
+           'title'=>fake()->title(),
+           'description'=>fake()->paragraph(),
+           'duedate'=>fake()->date(),
+           'status'=>$arrayValues[rand(0,2)],
+           'user_id'=>User::factory()->create()->id,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        User::factory(10)->create();
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Evans',
+            'email' => 'evans.nawu@gmail.com',
         ]);
+
+        $this->call(TaskSeeder::class);
     }
 }
