47993c8e8fad219ba898534e9a80c5444203794f
Evans Nawu <evans.nawu@wconsulting.mu>
2024-06-10 12:37:41 +0400

    fix: correct the field names

--- resources/views/tasks/partials/form.blade.php
+++ resources/views/tasks/partials/form.blade.php
@@ -1,7 +1,7 @@
 <div class="mb-4">
     <x-input-label for="title" :value="__('Title')" />
     <x-text-input id="title" name="title" type="text" class="mt-1 block w-full"
-        value="{{ $task->value ?? old('title') }} " />
+        value="{{ $task->title ?? old('title') }} " />
     <x-input-error :messages="$errors->first('title')" class="mt-2" />
 
 </div>
@@ -9,20 +9,22 @@
 <div class="mb-4">
     <x-input-label for="duedate" :value="__('Due Date')" />
     <x-text-input id="duedate" name="duedate" type="date" class="mt-1 block w-full"
-        value="{{ $task->value ?? old('duedate') }} " />
+        value="{{ $task->duedate ?? old('duedate') }} " />
     <x-input-error :messages="$errors->first('duedate')" class="mt-2" />
 
 </div>
 
+
 <div class="mb-4">
     <x-input-label for="status" :value="__('Status')" />
     <select class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
-        name="status" id="active">
+        name="status" id="status">
         <option value="" selected disabled>--Select One--</option>
 
-        @foreach ($statuses as $key => $status)
-            <option value="{{ $key }}" {{ (old('status') ?? $task->status) == $key ? 'selected' : '' }}>
-                {{ $status }}
+        @foreach ($statuses as $status)
+            <option value="{{ trim($status) }}"
+                {{ (old('status') ?? $task->status) == trim($status) ? 'selected' : '' }}>
+                {{ trim($status) }}
             </option>
         @endforeach
 
