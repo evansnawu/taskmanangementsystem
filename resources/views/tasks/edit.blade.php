cc02dec67c4be1e0ef4378df6164ed10b927b471
Evans Nawu <evans.nawu@wconsulting.mu>
2024-06-10 12:37:07 +0400

    feat: add task blade edit form

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Task') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+            <form method="post" action="{{ route('tasks.update',$task->id) }}" class=" grid grid-cols-1 lg:grid-cols-2 gap-4 p-6">
+                @csrf
+                @method('PATCH')
+                @include('tasks.partials.form')
+
+                <div class="flex items-center gap-4">
+                    <x-primary-button>{{ __('Save') }}</x-primary-button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</x-app-layout>
