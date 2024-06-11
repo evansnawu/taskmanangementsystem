5a934904d34729106eb2d2496495cd780c9155d9
Evans Nawu <evans.nawu@wconsulting.mu>
2024-06-11 16:21:05 +0400

    test: recofigure pest for unit test

--- tests/Pest.php
+++ tests/Pest.php
@@ -16,6 +16,9 @@ use Tests\TestCase;
 
 uses(TestCase::class, RefreshDatabase::class)->in('Feature');
 
+uses(
+    TestCase::class, RefreshDatabase::class
+)->in('Unit'); //refers to Tests\Unit
 /*
 |--------------------------------------------------------------------------
 | Expectations
