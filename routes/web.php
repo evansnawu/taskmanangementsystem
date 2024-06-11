23f38f04a829145e9b6dedcc7b5bff1bd86f8075
Evans Nawu <evans.nawu@wconsulting.mu>
2024-06-11 09:12:29 +0400

    refactor: remove the dashboard links

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,14 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/',[TaskController::class,'index']);
     Route::resource('tasks', TaskController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
