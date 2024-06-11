cb785920a18f912fc603c2dab79879cd1e471004
Evans Nawu <evans.nawu@wconsulting.mu>
2024-06-11 16:18:19 +0400

    style: add wave style at the botttom

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,7 +44,7 @@
 
             @if (Route::has('register'))
                 <a href="{{ route('register') }}"
-                    class="inline-flex items-center px-4 py-2 ml-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    class="inline-flex items-center px-4 ml-6 py-2 bg-gray-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                     Sign Up
                 </a>
             @endif
@@ -53,4 +53,9 @@
             </x-primary-button>
         </div>
     </form>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#22c67f" fill-opacity="1" d="M0,288L48,272C96,256,192,224,288,197.3C384,171,480,149,576,165.3C672,181,768,235,864,250.7C960,267,1056,245,1152,250.7C1248,256,1344,288,1392,304L1440,320L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
+      </svg>
+
 </x-guest-layout>
